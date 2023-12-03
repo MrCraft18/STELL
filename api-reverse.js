7311d5030ea363108fddb0c299d649dc34220ad8
MrCraft18 <mariocaden@gmail.com>
2023-12-02 22:12:44 -0600

    login and fetchPhoneNumbers functions complete

--- api-reverse.js
+++ api-reverse.js
@@ -1,38 +1,98 @@
+const { response } = require('express')
+
 const axios = require('axios').create({
     withCredentials: true,
     baseURL: 'https://salesgodcrm.net/api/'
-  });
-  
-  axios.get('sanctum/csrf-cookie').then(response => {
+})
 
+const cookies = {}
 
-    let XRSF_TOKEN, SESSION, COOKIE;
+const salesGodCRM = {
+    login: async (email, password) => {
+        try {
+            await axios.get('sanctum/csrf-cookie')
+            .then(response => {
+                updateSession(response)
+            })
+
+            await axios.post('auth/login', {"email": email,"password": password}, {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    ...cookies,
+                    "Referer": "https://salesgodcrm.net/auth/login",
+                    "Referrer-Policy": "strict-origin-when-cross-origin"
+                  } 
+            })
+            .then(response => {
+                updateSession(response)
+            })
+        } catch (error) {
+            if (error.response && error.response.data) {
+                throw new Error(`Error during login process: ${JSON.stringify(error.response.data)}`)
+            } else {
+                throw new Error(`Error during login process: ${error}`)
+            }
+        }
+    },
+    fetchPhoneNumbers: async () => {
+        try {
+            return await axios.get('/phoneNumber/fetchPhoneNumbers?limit=10000&sort=is_primary', {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    ...cookies,
+                    "Referer": "https://salesgodcrm.net/phone_numbers",
+                    "Referrer-Policy": "strict-origin-when-cross-origin"
+                }
+            })
+            .then(response => {
+                if (response.data.tableData.last_page < 1) {
+                    console.log('Bro you have WAAAAAAAAY to many phone numbers')
+                }
+
+                return response.data.tableData.data
+            })
+        } catch (error) {
+            if (error.response && error.response.data) {
+                throw new Error(`Error fetching Phone Numbers: ${JSON.stringify(error.response.data)}`)
+            } else {
+                throw new Error(`Error fetching Phone Numbers: ${error}`)
+            } 
+        }
+    }
+}
+
+function updateSession(response) {
+    let XRSF_TOKEN, SESSION, COOKIE
 
     response.headers['set-cookie'].forEach(cookie => {
-      if (cookie.startsWith('XSRF-TOKEN=')) {
-        XRSF_TOKEN = cookie.split(';')[0] + '; '
-      } else if (cookie.startsWith('salesgod_crm_session=')) {
-        SESSION = cookie.split(';')[0] + '; '
-      } else {
-        COOKIE = cookie.split(';')[0] + ';'
-      }
-    })
 
-    console.log(XRSF_TOKEN.replace('%3D; ', '='))
+        if (cookie.startsWith('XSRF-TOKEN=')) {
+              XRSF_TOKEN = cookie.split(';')[0] + '; '
+            } else if (cookie.startsWith('salesgod_crm_session=')) {
+              SESSION = cookie.split(';')[0] + '; '
+            } else {
+              COOKIE = cookie.split(';')[0] + ';'
+            }
+        })
 
-    axios.post('auth/login', {"email":"jacobwalkersolutions@gmail.com","password":"Godsgotthis#1"}, {
-        headers: {
-            "Accept": "application/json",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Content-Type": "application/json",
-            "X-XSRF-TOKEN": XRSF_TOKEN.replace('XSRF-TOKEN=', '').replace('%3D; ', '='),
-            "Cookie": (XRSF_TOKEN + SESSION + COOKIE),
-            "Referer": "https://salesgodcrm.net/auth/login",
-            "Referrer-Policy": "strict-origin-when-cross-origin"
-          }
-    })
-    .then(response => console.log(response.data))
-  })
-  .catch(error => {
-    console.error('Error fetching CSRF token:', error);
-  });
+        cookies['X-XSRF-TOKEN'] = XRSF_TOKEN.replace('XSRF-TOKEN=', '').replace('%3D; ', '=')
+        cookies['Cookie'] = (XRSF_TOKEN + SESSION + COOKIE)
+}
+
+(async function () {
+    try {
+        console.log('Before Login')
+        
+        await salesGodCRM.login("jacobwalkersolutions@gmail.com", "Godsgotthis#1")
+
+        console.log("Logged Into SalesGodCRM Successfully")
+    
+        const phoneNumbers = await salesGodCRM.fetchPhoneNumbers()
+
+        console.log(phoneNumbers)
+    } catch (error) {
+        console.error(error)
+    }
+})()
