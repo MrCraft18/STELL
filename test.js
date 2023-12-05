652a77f25e01990dfb306d8a2e1ae126f6f38c4c
MrCraft18 <mariocaden@gmail.com>
2023-12-05 17:41:48 -0600

    salesgodcrm lib progress

--- test.js
+++ test.js
@@ -1,3 +1,33 @@
-axios = require("axios")
+const WebSocket = require('ws')
 
-axios.post("http://localhost:6100/send", {message: "Same Time?", number: "8176737349"})
+ws = new WebSocket('wss://salesgodcrm.net:3000/socket.io/?EIO=4&transport=websocket', {
+                headers: {
+                    'Origin': 'https://salesgodcrm.net'
+                }
+            })
+
+            ws.on('open', ()  => {
+                console.log('connected ws')
+
+                ws.send(40)
+            })
+
+            ws.on('message', (data) => {
+                const message = data.toString()
+
+                console.log(message)
+
+                if (message == 2) {
+                    ws.send(3)
+                }
+
+                if (message.startsWith(40)) {
+                    ws.send('42["userConnected",[1,"user"]]')
+                }
+
+                if (message.startsWith(42)) {
+                    string = message.substring(2)
+
+                    console.log(JSON.parse(string))
+                }
+            })
