f1ee248ab1943af8daab45ae4970d452b182cf3a
MrCraft18 <mariocaden@gmail.com>
2023-12-15 13:32:32 -0600

    start redo database

--- test.js
+++ test.js
@@ -1,33 +1,5 @@
-const WebSocket = require('ws')
+const fs = require('fs')
 
-ws = new WebSocket('wss://salesgodcrm.net:3000/socket.io/?EIO=4&transport=websocket', {
-                headers: {
-                    'Origin': 'https://salesgodcrm.net'
-                }
-            })
+const files = fs.readdirSync('.')
 
-            ws.on('open', ()  => {
-                console.log('connected ws')
-
-                ws.send(40)
-            })
-
-            ws.on('message', (data) => {
-                const message = data.toString()
-
-                console.log(message)
-
-                if (message == 2) {
-                    ws.send(3)
-                }
-
-                if (message.startsWith(40)) {
-                    ws.send('42["userConnected",[1,"user"]]')
-                }
-
-                if (message.startsWith(42)) {
-                    string = message.substring(2)
-
-                    console.log(JSON.parse(string))
-                }
-            })
+console.log(files)
