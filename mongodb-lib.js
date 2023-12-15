4c6b8c904ec04613343009f089bffd7db8b73849
MrCraft18 <mariocaden@gmail.com>
2023-12-15 10:42:18 -0600

    Cleanup

--- mongodb-lib.js
+++ mongodb-lib.js
@@ -8,81 +8,81 @@ const client = new MongoClient(url);
 
 
 const connectDatabase = async () => {
-  await client.connect()
+    await client.connect()
 }
 
 const closeDatabase = async () => {
-  await client.close()
+    await client.close()
 }
 
 
 
 const addNewRecords = async (records) => {
-  const dataBase = client.db('Main')
-  const collection = dataBase.collection('unsentRecords')
+    const dataBase = client.db('Main')
+    const collection = dataBase.collection('unsentRecords')
 
-  await collection.insertMany(records)
-  console.log("Added New Records")
+    await collection.insertMany(records)
+    console.log("Added New Records")
 }
 
 
 
 const getUnsentRecords = async () => {
-  const dataBase = client.db('Main')
-  const collection = dataBase.collection('unsentRecords')
+    const dataBase = client.db('Main')
+    const collection = dataBase.collection('unsentRecords')
 
-  const unsentRecords = await collection.find({}).project({ _id: 0 }).toArray()
+    const unsentRecords = await collection.find({}).project({ _id: 0 }).toArray()
 
-  return unsentRecords
+    return unsentRecords
 }
 
 
 
 const removeUnsentRecord = async (record) => {
-  const dataBase = client.db('Main')
-  const collection = dataBase.collection('unsentRecords')
+    const dataBase = client.db('Main')
+    const collection = dataBase.collection('unsentRecords')
 
-  await collection.deleteMany({ phoneNumber: record.phoneNumber})
+    await collection.deleteMany({ phoneNumber: record.phoneNumber })
 }
 
 
 
 const addNewConversation = async (record) => {
-  const dataBase = client.db('Main')
-  const collection = dataBase.collection('conversations')
+    const dataBase = client.db('Main')
+    const collection = dataBase.collection('conversations')
 
-  await collection.insertMany([record])
-  console.log(`Created Conversation for ${record.phoneNumber}`)
+    await collection.insertMany([record])
+    console.log(`Created Conversation for ${record.phoneNumber}`)
 }
 
 
 
 const getConversation = async (from) => {
-  const dataBase = client.db('Main')
-  const collection = dataBase.collection('conversations')
+    const dataBase = client.db('Main')
+    const collection = dataBase.collection('conversations')
 
-  const conversation = await collection.find({ phoneNumber : from }).project({ _id: 0 }).toArray()
+    const conversation = await collection.find({ phoneNumber: from }).project({ _id: 0 }).toArray()++
 
-  return conversation[0]
+    return conversation[0]
 }
 
 
 
 const updateConversation = async (updatedRecord) => {
-  const dataBase = client.db('Main')
-  const collection = dataBase.collection('conversations')
+    const dataBase = client.db('Main')
+    const collection = dataBase.collection('conversations')
 
-  await collection.updateOne({ phoneNumber: updatedRecord.phoneNumber }, { $set: { conversation: updatedRecord.conversation, conversationLabel: updatedRecord.conversationLabel, webhook: updatedRecord.webhook } })
-  console.log('Updated Conversation')
+    await collection.updateOne({ phoneNumber: updatedRecord.phoneNumber }, { $set: { conversation: updatedRecord.conversation, conversationLabel: updatedRecord.conversationLabel, webhook: updatedRecord.webhook } })
+    console.log('Updated Conversation')
 }
 
 
 
 const deleteConversation = async (record) => {
-  const dataBase = client.db('Main')
-  const collection = dataBase.collection('conversations')
+    const dataBase = client.db('Main')
+    const collection = dataBase.collection('conversations')
 
-  await collection.deleteMany({ phoneNumber: record.phoneNumber})
+    await collection.deleteMany({ phoneNumber: record.phoneNumber })
 }
 
 
@@ -95,13 +95,13 @@ const deleteConversation = async (record) => {
 
 
 module.exports = {
-  connectDatabase,
-  closeDatabase,
-  addNewRecords,
-  getUnsentRecords,
-  removeUnsentRecord,
-  addNewConversation,
-  getConversation,
-  updateConversation,
-  deleteConversation
+    connectDatabase,
+    closeDatabase,
+    addNewRecords,
+    getUnsentRecords,
+    removeUnsentRecord,
+    addNewConversation,
+    getConversation,
+    updateConversation,
+    deleteConversation
 }
