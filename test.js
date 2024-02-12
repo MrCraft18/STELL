34135c0a7a3dc3326da3bc5c32a2b3b1ae26cde2
MrCraft18 <mariocaden@gmail.com>
2024-02-12 16:32:58 -0600

    key update

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log(new Date().toLocaleString())
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
+const client = new MongoClient(process.env.MONGODB_URI)
+
+const unsentRecords = client.db('STELL').collection('unsentRecords')
+const conversationsCollection = client.db('STELL').collection('conversations')
+
+
+async function main() {
+    await conversationsCollection.updateMany({stage: -1}, {$set: {stage: 'dropped'}})
+}
+main()
