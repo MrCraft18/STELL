31ff9774de023281da39dd9d82ab7bffd731b6ce
MrCraft18 <mariocaden@gmail.com>
2024-02-15 23:53:34 -0600

    Made page layout after file is imported

--- public/add-list/script.js
+++ public/add-list/script.js
@@ -45,21 +45,42 @@ function uploadClick() {
 function inputChange(element) {
     document.getElementById('file-upload').remove()
 
-    document.getElementById('setup-container').style.display = 'block'
+    document.getElementById('setup-container').style.display = 'flex'
     document.getElementById('dynamic-container').style.display = 'block'
 
     handleFile(element.files[0])
 }
 
 function handleFile(file) {
-    let records = []
-
     Papa.parse(file, {
         header: true,
-        complete: (results, file) => {
-            console.log(results.data.length)
+        complete: (results) => {
+            const recordsArray = results.data
 
-            console.log(getRandomElements(results.data, 5))
+            console.log(recordsArray.length)
+
+            const rowAmount = 8
+
+            const csvGridElement = document.getElementById('csv-grid')
+            csvGridElement.style.gridTemplateColumns = `repeat(${Object.keys(recordsArray[0]).length}, 1fr)`
+
+            Object.keys(recordsArray[0]).forEach(header => {
+                const headerCellElement = document.createElement('div')
+                headerCellElement.classList.add('grid-item')
+                headerCellElement.offsetHeight = `calc(${csvGridElement.offsetHeight}px / ${rowAmount})`
+                headerCellElement.innerText = header
+                csvGridElement.appendChild(headerCellElement)
+            })
+
+            recordsArray.slice(0, rowAmount - 1).forEach(row => {
+                Object.values(row).forEach(value => {
+                    const cellElement = document.createElement('div')
+                    cellElement.classList.add('grid-item')
+                    cellElement.offsetHeight = `calc(${csvGridElement.offsetHeight}px / ${rowAmount})`
+                    cellElement.innerText = value
+                    csvGridElement.appendChild(cellElement)
+                })
+            })
         }
     })
 }
