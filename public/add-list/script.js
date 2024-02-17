3225ce88591e9ef871e61a8dd48908e3f752d202
MrCraft18 <mariocaden@gmail.com>
2024-02-16 20:56:00 -0600

    select headers for fields functionality and more grid rework

--- public/add-list/script.js
+++ public/add-list/script.js
@@ -42,7 +42,7 @@ function uploadClick() {
     document.getElementById('file-input').click()
 }
 
-function inputChange(element) {
+function fileInputChange(element) {
     document.getElementById('file-upload').remove()
 
     document.getElementById('setup-container').style.display = 'flex'
@@ -52,6 +52,7 @@ function inputChange(element) {
 }
 
 let importedFile
+let exampleRecord
 
 function handleFile(file) {
     importedFile = file
@@ -70,7 +71,7 @@ function handleFile(file) {
             csvGridElement.style.gridTemplateColumns = `repeat(${headersArray.length}, 250px)`
 
             const headersString = headersArray.map(header => `
-                <div class='grid-item'>${header}</div>
+                <div class='grid-item header'>${header}</div>
             `).join('')
 
             const gridItemsString = recordsArray.map(row => {
@@ -84,21 +85,114 @@ function handleFile(file) {
             const dropDownContainerElements = document.querySelectorAll('.dropdown-container')
             dropDownContainerElements.forEach(dropDownContainerElement => {
                 dropDownContainerElement.innerHTML = fields.map(field => `
-                    <div class="dropdown-item">${field}</div>
+                    <div class="dropdown-item" onclick="dropDownItemClick(this)">${field}</div>
                 `).join('')
             })
         }
     })
+
+    Papa.parse(file, {
+        preview: 1,
+        header: true,
+        complete: (results) => {
+            exampleRecord = results.data[0]
+            console.log(exampleRecord)
+        }
+    })
 }
 
-function getRandomElements(arr, numElements) {
-    for (let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1)); 
-        [arr[i], arr[j]] = [arr[j], arr[i]]; 
+
+
+function inputChange(element) {
+    console.log(element.value)
+
+    const inputContainerElement = element.parentNode
+
+    const dropDownItemElements = inputContainerElement.querySelectorAll('.dropdown-item')
+    dropDownItemElements.forEach(dropDownItemElement => {
+        if (!dropDownItemElement.innerText.toLowerCase().startsWith(element.value.toLowerCase())) {
+            dropDownItemElement.style.display = 'none'
+        } else {
+            dropDownItemElement.style.display = ''
+        }
+    })
+}
+
+function inputFocus(element) {
+    element.value = ''
+
+    const inputContainerElement = element.parentNode
+
+    const dropDownItemElements = inputContainerElement.querySelectorAll('.dropdown-item')
+    dropDownItemElements.forEach(dropDownItemElement => {
+        dropDownItemElement.style.display = ''
+    })
+    
+    const dropDownContainerElement = inputContainerElement.querySelector('.dropdown-container')
+    dropDownContainerElement.style.display = 'block'
+}
+
+function inputBlur(element) {
+    const inputContainerElement = element.parentNode
+    
+    const dropDownContainerElement = inputContainerElement.querySelector('.dropdown-container')
+    setTimeout(() => {
+        dropDownContainerElement.style.display = 'none'
+    }, 100);
+}
+
+
+
+let selectedHeaders = {
+    firstName: null,
+    lastName: null,
+    streetAddress: null,
+    city: null,
+    state: null,
+    zip: null,
+    phoneNumber: null
+}
+
+function dropDownItemClick(element) {
+    const headerName = element.innerText
+
+    const inputElement = element.parentNode.parentNode.querySelector('.input')
+
+    switch (inputElement.id) {
+        case 'first-name':
+            inputElement.value = exampleRecord[headerName].split(' ').map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
+            selectedHeaders.firstName = headerName
+            break
+
+        case 'last-name':
+            inputElement.value = exampleRecord[headerName].split(' ').map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
+            selectedHeaders.lastName = headerName
+            break
+
+        case 'street-address':
+            inputElement.value = exampleRecord[headerName].split(' ').map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
+            selectedHeaders.streetAddress = headerName
+            break
+
+        case 'city':
+            inputElement.value = exampleRecord[headerName].split(' ').map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
+            selectedHeaders.city = headerName
+            break
+
+        case 'state':
+            inputElement.value = exampleRecord[headerName].toUpperCase()
+            selectedHeaders.state = headerName
+            break
+
+        case 'zip':
+            inputElement.value = exampleRecord[headerName]
+            selectedHeaders.zip = headerName
+            break
+
+        case 'phone-number':
+            const numberDigits = exampleRecord[headerName].replace(/\D/g, '')
+            inputElement.value = numberDigits.length > 10 ? numberDigits.slice(1) : numberDigits
+            selectedHeaders.phoneNumber = headerName
+            break
     }
-
-    return arr.slice(0, numElements);
 }
-
-
-
