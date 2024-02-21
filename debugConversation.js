d3b245a168f52400f567826e098e58c4daf5410a
MrCraft18 <mariocaden@gmail.com>
2024-02-20 19:00:12 -0600

    Added speak more casually prompt to response prompts

--- debugConversation.js
+++ debugConversation.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 
-const masterName = 'Kieshaa Rose'
+const masterName = 'David Zulejkic'
 const masterNumber = '8176737349'
-const masterAddress = '1781 N Pratt Rd'
+const masterAddress = '1 Lasater Rd'
 const content = `Hello There, I am Jacob in Fort Worth. Is this still ${masterName.split(' ')[0]}?`
 
 axios.post('http://104.190.170.134:6102/debugConversation', {
