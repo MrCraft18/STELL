ccc3c3a87774f95278669ae0f262dcc3d0155570
MrCraft18 <mariocaden@gmail.com>
2024-02-28 23:38:14 -0600

    Changed debogConversation.js to use localhost

--- debugConversation.js
+++ debugConversation.js
@@ -5,7 +5,7 @@ const masterNumber = '8176737349'
 const masterAddress = '1 Lasater Rd'
 const content = `Hello There, I am Jacob in Fort Worth. Is this still ${masterName.split(' ')[0]}?`
 
-axios.post('http://104.190.170.134:6102/debugConversation', {
+axios.post('http://localhost:6102/debugConversation', {
     masterName,
     masterNumber,
     masterAddress,
